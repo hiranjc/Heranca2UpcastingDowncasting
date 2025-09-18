119d2a4b08194415882aa9affcf237a60e720025
Hiran Ferreti Baccos <hiranjc1@outlook.com>
2025-09-18 18:48:44 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Heranca2UpcastingDowncasting.Entities;
+
+namespace Heranca2UpcastingDowncasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc = new Account(1001, "Alex", 0.0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.00);
+
+            //UPCASTING
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.00);
+            Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.1);
+
+            //DOWNCASTING
+            if (acc3 is BusinessAccount)
+            {
+                BusinessAccount acc5 = acc3 as BusinessAccount;
+                acc5.Loan(200.00);
+                Console.WriteLine("Loan!");
+            }
+
+            if (acc3 is SavingsAccount)
+            {
+                SavingsAccount acc5 = acc3 as SavingsAccount;
+                acc5.UpdateBalance();
+                Console.WriteLine("Update!");
+            }
+        }
+    }
+}
